24b97e34ae28a19a1f4fb1ba7b86d86502052597
beyondthegame-zz <495133744@qq.com>
2020-10-28 16:21:03 +0800

    remove controller

--- MyProject/src/main/java/com/myproject/jpa/repository/RecoverRepository.java
+++
@@ -1,8 +0,0 @@
-package com.myproject.jpa.repository;
-
-import com.myproject.jpa.bean.RecoverDataLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecoverRepository extends JpaRepository<RecoverDataLog, Long> {
-
-}
