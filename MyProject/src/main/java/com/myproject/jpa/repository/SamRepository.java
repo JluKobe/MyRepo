d245f5714942cfc877f4642cd2373939f78fcc58
beyondthegame-zz <495133744@qq.com>
2020-10-28 15:03:00 +0800

    删除MyProject

--- MyProject/src/main/java/com/myproject/jpa/repository/SamRepository.java
+++
@@ -1,7 +0,0 @@
-package com.myproject.jpa.repository;
-
-import com.myproject.jpa.bean.Sam;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SamRepository extends JpaRepository<Sam, Long> {
-}
--- MyProject/src/main/java/com/myproject/service/IWord.java
+++
@@ -1,7 +0,0 @@
-package com.myproject.service;
-
-import com.myproject.vo.WordVo;
-
-public interface IWord {
-    long addWord(WordVo wordVo);
-}
