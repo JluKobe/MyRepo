adc211223e9ba318e80439454c91d20f6b471345
beyondthegame-zz <495133744@qq.com>
2020-11-04 14:59:48 +0800

    add postprocess test

---
+++ SpringProject/src/main/java/com/spring/main/TestPostProcess.java
@@ -0,0 +1,14 @@
+package com.spring.main;
+
+import com.spring.bean.lifecycle.PostProcessBean;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestPostProcess {
+    public static void main(String[] args) {
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("BeanPostProcess.xml");
+        PostProcessBean bean = (PostProcessBean) context.getBean("postProcessBean");
+        bean.getMessage();
+        context.registerShutdownHook();
+    }
+}
