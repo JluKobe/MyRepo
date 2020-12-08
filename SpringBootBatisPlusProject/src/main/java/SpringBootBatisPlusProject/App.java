7f6b2dd23c21836fb4dd40918d5fd16fdbb088f9
zhengzhao <zhengzhao@jilinxiangyun.com>
2020-12-08 09:24:42 +0800

    add project

---
+++ SpringBootBatisPlusProject/src/main/java/SpringBootBatisPlusProject/App.java
@@ -0,0 +1,13 @@
+package SpringBootBatisPlusProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
