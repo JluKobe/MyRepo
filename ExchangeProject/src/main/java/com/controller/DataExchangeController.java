f90c0d625197c72cbcedb8ee7f1da608c5e24a8b
zhengzhao <zhengzhao@jilinxiangyun.com>
2020-12-14 09:55:28 +0800

    delete redundancy code

--- ExchangeProject/src/main/java/com/controller/DataExchangeController.java
+++ ExchangeProject/src/main/java/com/controller/DataExchangeController.java
@@ -12,8 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/dataExchangeController")
 @Api(value = "DataExchangeController", tags = {"data exchange controller"})
