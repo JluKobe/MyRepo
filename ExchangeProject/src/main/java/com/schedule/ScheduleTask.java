64c7717cbac996608f8ff02ff2a134daf988c52f
zhengzhao <zhengzhao@jilinxiangyun.com>
2020-12-30 09:00:33 +0800

    modify the scheduled task

--- ExchangeProject/src/main/java/com/schedule/ScheduleTask.java
+++ ExchangeProject/src/main/java/com/schedule/ScheduleTask.java
@@ -3,9 +3,11 @@ package com.schedule;
 import com.alibaba.nacos.api.config.annotation.NacosValue;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.bean.entity.clean.CleanBasic;
+import com.bean.entity.igt.IgtTaskBasic;
 import com.config.DbContextHolder;
 import com.enums.DBTypeEnum;
 import com.repository.CleanBasicRepository;
+import com.repository.IgtBasicRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +15,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
+import java.util.Date;
 
 /**
  * 定时任务demo
@@ -28,6 +32,9 @@ public class ScheduleTask {
     @Autowired
     private CleanBasicRepository cleanBasicRepository;
 
+    @Autowired
+    private IgtBasicRepository igtBasicRepository;
+
 //    @NacosValue(value = "${exchange.taskHandleItem}", autoRefreshed = true)
 //    private String taskHandleItem;
 
@@ -37,11 +44,19 @@ public class ScheduleTask {
     //@Scheduled(fixedRate=5000)
     private void configureTasks() {
         System.out.println("执行静态定时任务时间: " + LocalDateTime.now());
-//        log.info("执行静态定时任务时间, {}", LocalDateTime.now());
-        DbContextHolder.setDbType(DBTypeEnum.db2);
-        CleanBasic cleanBasic = cleanBasicRepository.selectOne(Wrappers.<CleanBasic>lambdaQuery()
-                .eq(CleanBasic::getTaskhandleitem, ""));
-        System.out.println(cleanBasic.getTaskname());
-        System.out.println(cleanBasic.getDeptname());
+        log.info("执行静态定时任务时间, {}", LocalDateTime.now());
+        DbContextHolder.setDbType(DBTypeEnum.db1);
+
+        IgtTaskBasic igtTaskBasic = igtBasicRepository.selectOne(Wrappers.<IgtTaskBasic>lambdaQuery()
+                .eq(IgtTaskBasic::getTaskHandleItem, "11220000MB15280770200017203000001"));
+
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String currentTime = sdf.format(date);
+        igtTaskBasic.setUpdateTime(currentTime);
+
+        igtBasicRepository.update(igtTaskBasic, Wrappers.<IgtTaskBasic>lambdaQuery()
+                .eq(IgtTaskBasic::getTaskHandleItem, "11220000MB15280770200017203000001"));
+        log.info("current time : {}", currentTime);
     }
 }
