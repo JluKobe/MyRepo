31a5a0265cad4c908852a50dd208b2c98f70d960
zhengzhao <zhengzhao@jilinxiangyun.com>
2021-01-04 15:24:14 +0800

    modify the controller

--- ExchangeProject/src/main/java/com/controller/DataExchangeController.java
+++ ExchangeProject/src/main/java/com/controller/DataExchangeController.java
@@ -6,12 +6,8 @@ import com.bean.vo.ExchangeTaskHandleItemVo;
 import com.service.IDataExchange;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.*;
 
 @RestController
 @RequestMapping("/dataExchangeController")
@@ -33,45 +29,5 @@ public class DataExchangeController {
     public ResultBean<Boolean> myTest(@RequestBody ExchangeTaskHandleItemVo vo) {
         iDataExchange.myTest(vo);
         return new ResultBean<>(true);
-    }
-
-    @PostMapping(value = "/upload")
-    public ResultBean<Boolean> update(@RequestParam("file") MultipartFile file) throws IOException {
-        String fileName = transferFileName(file.getOriginalFilename());
-        String suffixName = fileName.substring(fileName.lastIndexOf("."));
-        InputStream fileStream = null;
-        File tempFile = File.createTempFile("temp_" + fileName, suffixName);
-        file.transferTo(tempFile);
-        tempFile.deleteOnExit();
-        fileStream = new FileInputStream(tempFile);
-
-        InputStreamReader isr = new InputStreamReader(fileStream);
-        BufferedReader br = new BufferedReader(isr);
-        String s = br.readLine();
-        while(s != null) {
-            System.out.println(s);
-            s = br.readLine();
-        }
-
-        return new ResultBean<>(true);
-    }
-
-    public static String transferFileName(String oldFileName) {
-        if (StringUtils.isEmpty(oldFileName)) {
-            return oldFileName;
-        }
-        oldFileName = oldFileName.trim();
-        //判断是否为IE浏览器的文件名，IE浏览器下文件名会带有盘符信息
-        // Check for Unix-style path
-        int unixSep = oldFileName.lastIndexOf('/');
-        // Check for Windows-style path
-        int winSep = oldFileName.lastIndexOf('\\');
-        // Cut off at latest possible point
-        int pos = (winSep > unixSep ? winSep : unixSep);
-        if (pos != -1) {
-            // Any sort of path separator found...
-            oldFileName = oldFileName.substring(pos + 1);
-        }
-        return oldFileName;
     }
 }
