0fc7292cfc8eea9eb3b94ccaa0943f1b583e7145
zhengzhao <zhengzhao@jilinxiangyun.com>
2021-01-19 18:44:49 +0800

    add the connection demo of zookeeper

---
+++ ZookeeperProject/src/main/java/zookeeper/ConnectionDemo.java
@@ -0,0 +1,39 @@
+package zookeeper;
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * zookeeper连接测试demo
+ */
+public class ConnectionDemo {
+    public static void main(String[] args) {
+        try {
+            final CountDownLatch countDownLatch=new CountDownLatch(1);
+            ZooKeeper zooKeeper=
+                    new ZooKeeper("192.168.58.131:2181," +
+                            "192.168.58.132:2181,192.168.58.133:2181",
+                            4000, new Watcher() {
+                        @Override
+                        public void process(WatchedEvent event) {
+                            if(Event.KeeperState.SyncConnected==event.getState()){
+                                //如果收到了服务端的响应事件，连接成功
+                                countDownLatch.countDown();
+                            }
+                        }
+                    });
+            countDownLatch.await();
+            //CONNECTED
+            System.out.println(zooKeeper.getState());
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
